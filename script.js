f0cb98b4f857c43a400d61cc427e7cca6cc45838
Kurt Justin J. Malubay <justinkurt837@gmail.com>
2025-04-06 00:36:52 +0800

    fixes

--- script.js
+++ script.js
@@ -3,19 +3,16 @@ function showMenu(){
     let menu = document.getElementById("Menu");
     let body = document.getElementById("body");
     let btn1 = document.getElementById("btnMenu");
-    let btn2 = document.getElementById("btnClose");
     if (!show){
         menu.style.display = "block";
         show = true;
         btn1.style.display = "none";
         body.style.overflow = "hidden";
-        btn2.style.display = "block";
     }
     else{
         menu.style.display = "none";
         show = false;
         btn1.style.display = "block";
         body.style.overflow = "auto";
-        btn2.style.display = "none";
     }
 }
